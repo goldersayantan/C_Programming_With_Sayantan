ad2efc698175ff7f3d8b4e7fc442463b6de532df
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-02 23:17:15 +0530

    Update

---
+++ Division Of Two Numbers _ 5.c
@@ -0,0 +1,11 @@
+// Division of Two numbers...
+# include <stdio.h>
+int main()    {
+    int num1, num2;
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+    printf("Enter the second number:");
+    scanf("%d",&num2);
+    printf("The Division of %d and %d is: %f", num1, num2,((float)num1/num2));
+ return 0;
+ }
