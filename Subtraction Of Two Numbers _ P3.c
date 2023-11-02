fc2cdeba691d056324ceb79828cb7543e330f325
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-02 23:09:30 +0530

    Update

---
+++ Subtraction Of Two Numbers _ P3.c
@@ -0,0 +1,11 @@
+// Subtraction of Two numbers...
+# include <stdio.h>
+int main()    {
+    int num1, num2;
+    printf("Enter the first number:");
+    scanf("%d",&num1);
+    printf("Enter the second number:");
+    scanf("%d",&num2);
+    printf("The Subtraction of %d and %d is: %d", num1, num2, ( num1-num2));
+ return 0;
+ }
