4eae5117ddb0a8bc480b6893af2ac7f60cbdd580
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-04 11:39:51 +0530

    Update

---
+++ Check Character With Help Of ASCII Value.c
@@ -0,0 +1,9 @@
+// Check A Character With Help Of ASCII Value...
+#include<stdio.h>
+int main()  {
+    int num;
+    printf("Enter a number:");
+    scanf("%d", &num);
+    printf("The character is: %c",((char) num));
+return 0;
+}
