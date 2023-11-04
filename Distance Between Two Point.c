4f065a4f8d25654251895249c60dce33d5d68cd6
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-04 13:40:08 +0530

    Update Distance Between Two Point.c

--- Distance Between Two Point.c
+++ Distance Between Two Point.c
@@ -5,10 +5,10 @@ int main()    {
     int x1, x2, y1, y2;
     printf("Enter the value of x1:");
     scanf("%d",&x1);
-    printf("Enter the value of x2:");
-    scanf("%d",&x2);
     printf("Enter the value of y1:");
     scanf("%d",&y1);
+    printf("Enter the value of x2:");
+    scanf("%d",&x2);
     printf("Enter the value of y2:");
     scanf("%d",&y2);
     float distance = sqrt((pow((x2-x1),2)) + (pow((y2-y1),2)));
