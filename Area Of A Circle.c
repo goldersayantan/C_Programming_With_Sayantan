c8039b82b2adc8cb6c8e247ecb4894b380006c76
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-04 09:43:19 +0530

    Update

---
+++ Area Of A Circle.c
@@ -0,0 +1,9 @@
+// Area Of A Circle...
+#include<stdio.h>
+int main()  {
+    float rad;
+    printf("Enter the radius of a circle:");
+    scanf("%f",&rad);
+    printf("The area of a circle is: %f",((float) 3.14 * rad * rad));
+return 0;
+}
