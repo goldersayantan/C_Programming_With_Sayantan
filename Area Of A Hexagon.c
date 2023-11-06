aa62cef083e0109bb9fdcf52a6af0f151ee580a9
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-06 13:27:52 +0530

    Update

---
+++ Area Of A Hexagon.c
@@ -0,0 +1,9 @@
+// Area Of A Hexagon...
+#include<stdio.h>
+int main()    {
+    float a;
+    printf("Enter the values of side:");
+    scanf("%f",&a);
+    printf("Area of hexagon is:%f\n",((3*(1.732)/2)*(a*a)));
+return 0;
+}
