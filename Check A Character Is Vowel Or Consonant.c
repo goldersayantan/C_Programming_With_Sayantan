a20b59688133162e5b315ade3e83378cc4b38b1b
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-07 00:15:43 +0530

    Update

---
+++ Check A Character Is Vowel Or Consonant.c
@@ -0,0 +1,18 @@
+// Check A Character Entered By The User Is A Vowel Or Consonant...
+#include<stdio.h>
+int main()    {
+    char ch;
+    printf("Enter a Character:\n");
+    scanf("%c",&ch);
+    if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))    {
+    if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' 
+      || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+        printf("The character entered is a vowel");
+        }else      {
+        printf("The character entered is a consonant");
+        }
+    }else    {
+        printf("Invalid character Try Again");
+    }
+return 0;
+}
