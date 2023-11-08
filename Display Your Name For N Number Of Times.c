89fe27409570f748561afab266c06780ded92e32
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-08 23:07:44 +0530

    Update Display Your Name For N Number Of Times.c

--- Display Your Name For N Number Of Times.c
+++ Display Your Name For N Number Of Times.c
@@ -1,11 +1,29 @@
 // Print Your Name For N Number Of Times.c
 # include <stdio.h>
 int main()    {
-    int num;
+    int num1;
     printf("Enter the number:");
-    scanf("%d",&num);
-    for( int i=1; i<=num; i++ )    {
+    scanf("%d",&num1);
+    for( int i=1; i<=num1; i++ )    {
     printf("My Name Is Sayantan Golder\n");
     }
-return 0;
-}
+
+
+    int num2,i=1;
+    printf("Enter the number:");
+    scanf("%d", &num2);
+    do    {
+    printf("My Name Is Sayantan Golder\n"); 
+    i++;
+    }while( i <= num2 );
+    
+    
+    int num3, j=1;
+    printf("Enter the number:");
+    scanf("%d", &num3);
+    while(j <= num3)    {
+    printf("My Name Is Sayantan Golder\n");
+    j++;
+    }
+    return 0;
+    }
