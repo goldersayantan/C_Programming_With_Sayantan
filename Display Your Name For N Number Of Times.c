88d16ac1b5ab4c9ef94c24e40b514cbba4a24741
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-08 22:59:51 +0530

    Update

---
+++ Display Your Name For N Number Of Times.c
@@ -0,0 +1,11 @@
+// Print Your Name For N Number Of Times.c
+# include <stdio.h>
+int main()    {
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    for( int i=1; i<=num; i++ )    {
+    printf("My Name Is Sayantan Golder\n");
+    }
+return 0;
+}
