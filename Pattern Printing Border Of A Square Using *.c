c6380ff14f2faee8fa4207f7b36d86ef2aa0203d
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-12 00:10:43 +0530

    Update

---
+++ Pattern Printing Border Of A Square Using *.c
@@ -0,0 +1,24 @@
+//******
+//*    *
+//*    *
+//******
+//Print This Pattern For Specific Row And Column...
+# include <stdio.h>
+int main()    {
+    int row, column;
+    printf("Enter the row number:");
+    scanf("%d",&row);
+    printf("Enter the column number:");
+    scanf("%d",&column);
+    for( int i=1; i<=row; i++)    {
+        for( int j=1; j<=column; j++)    {
+        if( i == 1 || i == row || j == 1 || j == column)    {
+        printf("*");
+        }else    {
+        printf(" ");
+        }
+        }
+        printf("\n");
+    }
+return 0;
+}
