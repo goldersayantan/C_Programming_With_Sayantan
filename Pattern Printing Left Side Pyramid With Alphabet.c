bf0e1a68108da09034e4be02846a99d978737598
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-11 23:45:56 +0530

    Update

---
+++ Pattern Printing Left Side Pyramid With Alphabet.c
@@ -0,0 +1,18 @@
+//A
+//AB
+//ABC
+//ABCD
+//Print This Pattern For N Number Of Lines...
+# include <stdio.h>
+int main()    {
+    int line;
+    printf("Enter the line number:");
+    scanf("%d",&line);
+    for( int i =1; i <= line; i++)    {
+        for( int j =1; j <= i; j++)    {
+        printf("%c ",j+64);
+        }
+        printf("\n");
+    }
+return 0;
+}
