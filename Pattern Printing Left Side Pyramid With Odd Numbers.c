d018acd01035a9efdf2a58effffbe1594f57e904
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-11 23:39:27 +0530

    Update Pattern Printing Left Side Pyramid With Odd Numbers.c

--- Pattern Printing Left Side Pyramid With Odd Numbers.c
+++ Pattern Printing Left Side Pyramid With Odd Numbers.c
@@ -5,11 +5,11 @@
 //Print This Pattern For N Number Of Lines...
 # include <stdio.h>
 int main()    {
-    int num;
-    printf("Enter the number up to where to print:");
-    scanf("%d",&num);
-    for( int i =1; i <= num; i+=2)    {
-        for( int j =1; j<=i; j+=2)    {
+    int line;
+    printf("Enter the line number:");
+    scanf("%d",&line);
+    for( int i =1; i <= line; i++)    {
+        for( int j =1; j <= (2*i)-1; j+=2)    {
         if( j%2 == 0)
         continue;
         printf("%d ",j);
