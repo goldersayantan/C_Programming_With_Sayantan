87d7c4bcd158b0fb476773d127349b2930ad04cf
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-13 19:27:03 +0530

    Update

---
+++ Calculate The Product Of All The Elements In A Array.c
@@ -0,0 +1,17 @@
+// Calculate The Product Of All The Elements In A Array...
+# include <stdio.h>
+int main()    {
+    int num;
+    printf("Enter the number of numbers in an array: ");
+    scanf("%d",&num);
+    int product=1;
+    int arr[num];
+    int i;
+    for( i=0; i<num; i++)    {
+    printf("Enter the element number %d :",i+1);
+    scanf("%d",&arr[i]);
+    product = product*arr[i];
+    }
+    printf("The sum of all the numbers in array is: %d",product);
+return 0;
+}
