46faaf613b809d0e8e1cc898ef286f0e13fa9660
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-19 22:08:37 +0530

    Update

---
+++ Pattern Printing Right Side Pyramid With Alphabet.c
@@ -0,0 +1,17 @@
+// Pattern Printing Right Side Pyramid With Alphabet...
+# include <stdio.h>
+int main()    {
+    int line;
+    printf("Enter the number of lines: ");
+    scanf("%d",&line);
+    for( int i=1; i <=line; i++)    {
+        for( int j=(line-i); j>=1; j--)    {
+        printf(" ");
+        }
+        for( int k=1; k<= i; k++)    {
+        printf("%c",(k+64));
+        }
+        printf("\n");
+    }
+return 0;
+}
