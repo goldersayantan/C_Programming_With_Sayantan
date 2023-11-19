5cadb4924f57e32497a562a7bf52babcb19302ab
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-19 22:16:26 +0530

    Update

---
+++ Pattern Printing Centre Based Pyramid With Numbers.c
@@ -0,0 +1,17 @@
+// Pattern Printing Centre Based Pyramid With Numbers... 
+# include <stdio.h>
+int main()    {
+    int line;
+    printf("Enter the number of lines: ");
+    scanf("%d",&line);
+    for( int i=1; i<=line; i++)    {
+        for( int j=(line-i); j>=1; j--)    {
+        printf(" ");
+        }
+        for( int k=1; k<=(2*i)-1; k++)    {
+        printf("%d",k);
+        }
+        printf("\n");
+    }
+return 0;
+}
