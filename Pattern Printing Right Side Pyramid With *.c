4c8cba8e6a779e34e823b2775f8c850ea0216c6f
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-19 10:53:47 +0530

    Update Pattern Printing Right Side Pyramid With *.c

--- Pattern Printing Right Side Pyramid With *.c
+++ Pattern Printing Right Side Pyramid With *.c
@@ -5,7 +5,7 @@ int main()    {
     printf("Enter the number of lines: ");
     scanf("%d",&line);
     for( int i=1; i<= line; i++)    {
-        for( int j=1; j<=(line - i); j++)    {
+        for( int j=(line-i); j>=1; j--)    {
         printf(" ");
         }
         for( int k=1; k<=i; k++)    {
