f6f75f797dcbcdb0bd1cd4ad0858e01a59f3e8d2
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-11-23 14:15:17 +0530

    Update Check Two Numbers Are Amicable Or Not.c

--- Check Two Numbers Are Amicable Or Not.c
+++ Check Two Numbers Are Amicable Or Not.c
@@ -6,13 +6,19 @@ int main()    {
     scanf("%d",&num1);
     printf("Enter the second number:");
     scanf("%d",&num2);
-    int sum =0;
+    int sum1 =0;
+    int sum2 =0;
     for( int i=1; i< num1; i++)    {
     if( num1%i == 0)    {
-    sum = sum +i;
+    sum1 = sum1 +i;
     }
     }
-    if(sum == num2)    {
+    for( int j=1; j< num2; j++)    {
+    if( num2%j == 0)    {
+    sum2 = sum2 +j;
+    }
+    }
+    if(sum1 == num2 && sum2 == num1)    {
     printf("The numbers are amicable number");
     }else    {
     printf("The numbers are not amicable number");
