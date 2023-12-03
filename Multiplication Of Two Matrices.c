4babf7e51f83b6d19226a8d81b97389fc90d8513
SAYANTAN GOLDER <144215903+goldersayantan@users.noreply.github.com>
2023-12-03 18:13:52 +0530

    Update Multiplication Of Two Matrices.c

--- Multiplication Of Two Matrices.c
+++ Multiplication Of Two Matrices.c
@@ -1,40 +1,65 @@
 // Multiplication Of Two Matrices...
 # include <stdio.h>
 int main()    {
-    int row, col;
-    printf("Enter the number of row: ");
-    scanf("%d",&row);
-    printf("Enter the number of column: ");
-    scanf("%d",&col);
-    printf("For First Matrix:-\n");
-    int arr1[row][col];
-    int i,j;
-    for( i=0; i<row; i++)    {
-        for( j=0; j<col; j++)    {
-        printf("Enter the number in %d %d index: ",i+1,j+1);
+    int row1, col1;
+    printf("Enter the number of rows: ");
+    scanf("%d",&row1);
+    printf("Enter the number of columns: ");
+    scanf("%d",&col1);
+    int arr1[row1][col1];
+    for( int i=0; i<row1; i++)    {
+        for( int j=0; j<col1; j++)    {
+        printf("Enter the element in %d%d index: ",(i+1),(j+1));
         scanf("%d",&arr1[i][j]);
         }
     }
-    printf("\n");
+    for( int k=0; k<row1; k++)    {
+        for( int l=0; l<col1; l++)    {
+        printf("%d ",arr1[k][l]);
+        }
+        printf("\n");
+    }
     
-    printf("For Second Matrix:-\n");
-    int arr2[row][col];
-    int k,l;
-    for( k=0; k<row; k++)    {
-        for( l=0; l<col; l++)    {
-        printf("Enter the number in %d %d index: ",k+1,l+1);
-        scanf("%d",&arr2[k][l]);
+    int row2, col2;
+    printf("Enter the number of rows: ");
+    scanf("%d",&row2);
+    printf("Enter the number of columns: ");
+    scanf("%d",&col2);
+    int arr2[row2][col2];
+    for( int i=0; i<row2; i++)    {
+        for( int j=0; j<col2; j++)    {
+        printf("Enter the element in %d%d index: ",(i+1),(j+1));
+        scanf("%d",&arr2[i][j]);
         }
     }
-    printf("\n");
+    for( int k=0; k<row2; k++)    {
+        for( int l=0; l<col2; l++)    {
+        printf("%d ",arr2[k][l]);
+        }
+        printf("\n");
+    }
     
-    printf("After Addition Of Two Matrices:");
-    printf("The Sum Is:\n");
-    for( int m=0; m<row; m++)    {
-        for( int n=0; n<col;n++)    {
-        printf("%d ",(arr1[m][n] * arr2[m][n]));
+    if(col1 == row2)     {
+    printf("The Multiplication of these two matrices is: \n");
+    int cr= col1;
+    int rs[row1][col2];
+    for( int i=0; i<row1; i++)    {
+        for( int j=0; j<col2; j++)    {
+        rs[i][j]=0;
+        for( int k=0; k<cr; k++)    {
+        rs[i][j]=rs[i][j]+( arr1[i][k]*arr2[k][j] );
+        }
     }
-    printf("\n");
     }
-    return 0;
+    
+    for( int i=0; i<row1; i++)    {
+        for( int j=0; j<col2; j++)    {
+        printf("%d ",rs[i][j]);
+        }
+        printf("\n");
+    }
+    }else    {
+    printf("Multiplication not possible...");
+    }
+return 0;    
 }
